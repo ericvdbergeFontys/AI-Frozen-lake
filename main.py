6d438fc4781275875d162840c2056687c2e18a32
eric.vandenberge@gmail.com <i454038@fhict.nl>
2023-04-29 20:15:22 +0200

    small adjustments

--- main.py
+++ main.py
@@ -3,27 +3,21 @@ import gym
 from policy_evaluation import PolicyEvaluation, PolicyEvaluationConfig
 
 #==================== setup ====================#
-DEBUG = True #if you want the debug results for in between training, you can set DEBUG = True
-
+DEBUG = False #if you want the debug results for in between training, you can set DEBUG = True
 size = 4
+
 map_name = "8x8" if size == 8  else "4x4"
 env = gym.make('FrozenLake-v1', render_mode="human", map_name=map_name, is_slippery=False)
 env.reset()
 env.render()
 
 #==================== debugging and testing before creating anything ====================#
-# 4 x 4 board = 16 observations possible
-print("DEBUG: observation_space: {}".format(env.observation_space.n))
-
-# actions: left 0, down 1, right 2, up 3 = 4 possible actions
-print("DEBUG: action_space: {}".format(env.action_space.n)) 
-
+print("DEBUG: observation_space: {}".format(env.observation_space.n)) # 4 x 4 board = 16 observations possible
+print("DEBUG: action_space: {}".format(env.action_space.n)) # actions: left 0, down 1, right 2, up 3 = 4 possible actions
 # we can ask the environment at a particular state, when you want to perform action x, what the probabilities are
 # using env.P[state][action]
-state = 0
-action = 0
 # env.P[state, action] returns format (transition probability, next state, reward, is terminal state)
-print("DEBUG: determening probability for state {}, action {}: {}".format(state, action, env.P[state][action]))
+print("DEBUG: determening probability for state {}, action {}: {}".format(0, 0, env.P[0][0]))
 
 #==================== hyper parameters ====================#
 gamma = 0.9
@@ -44,32 +38,35 @@ policy_evaluation.evaluate()
 
 #==================== play ====================#
 print("INFO: Playing the game.......")
-observation = 0
+state = 0
 last_state_value = 0
 best_next_state = 0
 step_action = -1
 terminated = False
 
 while(terminated == False):
-    for action in env.P[observation]:
-        for probability, next_state, reward, isTerminalState in env.P[observation][action]:
+    # we are in a current state
+    # choose the next_state with the highest value in the value table.
+    for action in env.P[state]:
+        for probability, next_state, reward, _ in env.P[state][action]:
             if policy_evaluation.Vπ[next_state] > last_state_value: 
                 last_state_value = policy_evaluation.Vπ[next_state]
                 best_next_state = next_state
                
 
-    #map next state to action
-    if observation - 1 == best_next_state : step_action = 0 # left
-    if observation + 1 == best_next_state : step_action = 2 # right
-    if observation - size == best_next_state : step_action = 3 # up
-    if observation + size == best_next_state : step_action = 1 # down
+    # map next state to action
+    if state - 1 == best_next_state : step_action = 0 # left
+    if state + 1 == best_next_state : step_action = 2 # right
+    if state - size == best_next_state : step_action = 3 # up
+    if state + size == best_next_state : step_action = 1 # down
 
     #take action
     observation, reward, terminated, truncated, info = env.step(step_action)
+    state = observation
 
     #render and reset
     env.render()
-    best_next_state = 0
 
+print("INFO: Ended the game.......")
 env.close()
 
